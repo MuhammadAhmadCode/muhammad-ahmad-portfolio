8e58b5dec1794a555b6357f77fea6b3a07d88384
MuhammadAhmadCode <ma2565983@gmail.com>
2026-04-26 09:35:50 +0500

    footer created

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full text-center text-lg text-white/70 py-4 bg-[#2a2929]">
+      @ Created with 💖 by M. Ahmad with 🍵
+    </div>
+  );
+};
+
+export default Footer;
